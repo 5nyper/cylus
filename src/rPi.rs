142408ea5c41b903630846b8a17861ed608bd521
Sinatra <root@stackin.money>
2016-01-06 20:35:09 -0500

    Update rPi.rs

--- src/rPi.rs
+++ src/rPi.rs
@@ -1,14 +1,31 @@
+use std::mem;
+use std::ptr;
+use std::fs::OpenOptions;
+use std::os::unix::fs::OpenOptionsExt;
+
 const BCM2708_PERI_BASE: u32 = 0x20000000;
 const GPIO_BASE: u32 = BCM2708_PERI_BASE + 0x200000;
+const O_SYNC: u32 = 1052672;
 
-enum Void { } // void type
+enum Void {} // void type
 
 struct bcm2835_peripheral {
-    addr_p: u64,
+    addr_p: u32,
     mem_fd: i32,
     map: *mut Void,
     addr: *mut i32
 }
+
+
 fn main() {
-    let gpio = //continue later when you have time 
+    unsafe {let gpio = bcm2835_peripheral { addr_p: GPIO_BASE, mem_fd: 0, map: mem::uninitialized(), addr: ptr::null_mut()};}
+}
+
+fn map_peripheral(foo: bcm2835_peripheral) {
+    let file = OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .mode(O_SYNC)
+                    .open("/dev/mem");
+    //Finish when you have time
 }
