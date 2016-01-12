4ee34954103d7856079472c1fc429e1017719044
Sinatra <root@stackin.money>
2016-01-11 22:09:27 -0500

    silence warnings for sleep_ms

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+#![feature(core_intrinsics)]
+#![allow(deprecated)]
 extern crate mmap;
 
 use std::ptr;
 use std::fs::OpenOptions;
-use mmap::{MemoryMap, MapOption};
+use mmap::MemoryMap;
 
 #[path="pi.rs"]
-mod Pi;
+mod pi;
 
 fn main() {
-    let mut gpio = Pi::Bcm2835Peripheral { 
-                    addr_p: &Pi::GPIO_BASE, 
+    let mut gpio = pi::Bcm2835Peripheral { 
+                    addr_p: &pi::GPIO_BASE, 
                     mem_fd: OpenOptions::new().create(true).open("temp.txt").unwrap(), 
                     map: MemoryMap::new(1024, &[]).unwrap(), 
                     addr: ptr::null_mut()
@@ -20,11 +22,11 @@ fn main() {
         gpio.out_gpio(4);
 
         loop {
-          gpio.set_gpio(1u8 << 4);
+          gpio.set_gpio(1u32 << 4);
           std::thread::sleep_ms(1000);
 
-          gpio.clear_gpio(1u8 << 4);
+          gpio.clear_gpio(1u32 << 4);
           std::thread::sleep_ms(1000);
-      }
-  }
+        }
+    }
 }
