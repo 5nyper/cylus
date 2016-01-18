8b2d4f9b82a94e5616cad4d6a56a807b163dd251
Sinatra <root@stackin.money>
2016-01-17 22:22:16 -0500

    remove unsafe block

--- examples/blink.rs
+++ examples/blink.rs
@@ -4,16 +4,16 @@ extern crate cylus;
 use cylus::Cylus;
 
 fn main() {
-    println!("Starting");
+    println!("Starting..");
     let gpio = Cylus::new(24);
-    unsafe {
-        for _ in 1..10 {
-          println!("{}", gpio.read());
-          gpio.high();
-          std::thread::sleep_ms(1000);
-          println!("{}", gpio.read());
-          gpio.low();
-          std::thread::sleep_ms(1000);
-        }
+    for _ in 1..10 {
+        println!("{}", gpio.read());
+        gpio.high();
+        
+        std::thread::sleep_ms(1000);
+        println!("{}", gpio.read());
+        
+        gpio.low();
+        std::thread::sleep_ms(1000);
     }
 }
